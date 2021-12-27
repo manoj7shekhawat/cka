72992d7110c023eaa630e30056459e84a747448c
Manoj Shekhawat <manoj7shekhawat@gmail.com>
2021-12-27 14:23:27 +0100

    Update file

--- install_gui.sh
+++ install_gui.sh
@@ -10,6 +10,13 @@ sudo firewall-cmd --add-port=5901/tcp --permanent
 
 sudo firewall-cmd --reload
 
+sudo dnf install langpacks-en glibc-all-langpacks -y
+
+sudo localectl set-locale LANG=en_US.utf8
+
+echo export LC_CTYPE=en_US.utf8
+echo export LC_ALL=en_US.utf8
+
 echo Make sure to open 5901 in Ingress/Inbound rules
 
 echo I am done 
