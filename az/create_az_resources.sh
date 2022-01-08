99fe5b82ead6abb00c7572d8311fc483fc55343a
manoj7shekhawat <manoj7shekhawat@gmail.com>
2022-01-08 21:31:30 +0100

    master - added resource group name support

--- az/create_az_resources.sh
+++ az/create_az_resources.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
 # Create AZ resources
-# $0 RG
+# $0 ResourceGroupName
 # $0 Number of nodes
  
 if [ $# != 2 ]
 then
-	echo Usage\; $0 \<RG\/No of nodes\> adminPassword
+	echo Usage\; $0 \<ResourceGroupName\/No of nodes\> adminPassword
 else
-	if [ $1 = "RG" ]
+	if [[ ! $1 =~ ^[0-9]$ ]]
 	then
-		echo Creating RG\: cka and control-plane node
+		echo Creating ResourceGroup\: $1 and control-plane node
 
 		echo Step 1\: Logging in
 		az login
 
 		echo Step 2\: Creating Resource Group
-		az group create --name cka --location "Germany West Central"
+		az group create --name $1 --location "Germany West Central"
 
 		echo Step 3\: Creating control-plane VM
-		az deployment group create --resource-group cka --template-file template.json --parameters \@parameters.json --parameters adminPassword=$2
+		az deployment group create --resource-group $1 \
+--template-file template.json --parameters \@parameters.json \
+--parameters adminPassword=$2 \
+--parameters virtualNetworkName=$1-vnet \
+--parameters virtualMachineRG=$1 
 
 		echo Step 4\: Listing IP address
-		az vm list-ip-addresses -g cka -n control-plane
+		az vm list-ip-addresses -g $1 -n control-plane
 
 	else
 		echo Creating $1 nodes
