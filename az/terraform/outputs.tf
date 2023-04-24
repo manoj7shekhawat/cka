bf622762324fd9b12c981d34dacfa189bc9378bb
Manoj Shekhawat <manoj.shekhawat@sap.com>
2023-04-24 13:59:41 +0200

    centos-to-rhel - Added IP address in output

--- az/terraform/outputs.tf
+++ az/terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "linux_virtual_machine_names" {
   value = module.compute.linux_virtual_machine_names
 }
+
+output "linux_virtual_machine_public_ip_address" {
+  value = module.compute.linux_virtual_machine_public_ip_address
+}
+
+output "linux_virtual_machine_private_ip_address" {
+  value = module.compute.linux_virtual_machine_private_ip_address
+}
