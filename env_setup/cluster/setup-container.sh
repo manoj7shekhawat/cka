84ca76759ad53d216c180bde3ef1ed850c8348a5
Manoj Shekhawat <manoj.shekhawat@sap.com>
2023-04-25 08:40:17 +0200

    centos-to-rhel - Updated shell scripts

--- env_setup/cluster/setup-container.sh
+++ env_setup/cluster/setup-container.sh
@@ -5,6 +5,7 @@
 # This script is written for RHEL 8.5 OR CentOS Linux 8 VM on Azure Cloud
 # This is first part in setting up Kubernetes cluster
 # After this please run: setup-kubetools.sh
+# Last modified: 25-Apr-2023
 
 
 MYOS=$(hostnamectl | awk -F': ' '/Operating/ { print $2}')
@@ -17,23 +18,31 @@ fi
 if [[ $MYOS =~ ^Red[[:space:]]Hat[[:space:]]Enterprise[[:space:]]Linux[[:space:]]8* ]] || [[ $MYOS =~ ^CentOS[[:space:]]Linux[[:space:]]8$ ]]
 then
 	echo Working on: $MYOS
-	
-	echo Step 1: Installing vim yum-utils device-mapper-persistent-data lvm2
+
+	echo Step 1: Disabling swap space
+
+	swapoff -a
+
+  echo Step 2: Removing old packages
+
+  dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine podman runc
+
+	echo Step 3: Installing vim yum-utils device-mapper-persistent-data lvm2
 
 	dnf install -y vim yum-utils device-mapper-persistent-data lvm2
 
-	echo Step 2: Adding Docker repository
-	dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo 
+	echo Step 4: Adding Docker repository
+	dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 
-	echo Step 3: Installing Docker-CE
+	echo Step 5: Installing Docker-CE
 	dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-	echo Step 4: Creating required directories
+	echo Step 6: Creating required directories
 	[ ! -d /etc/docker ] && mkdir /etc/docker
 
 	mkdir -p /etc/systemd/system/docker.service.d
 
-	echo Step 5: Creating daemon.json under /etc/docker
+	echo Step 7: Creating daemon.json under /etc/docker
 
 	cat > /etc/docker/daemon.json <<- EOF
 	{
@@ -49,16 +58,16 @@ then
 	}
 	EOF
 
-	echo Step 6: Reloading daemon
+	echo Step 8: Reloading daemon
 	systemctl daemon-reload
 
-	echo Step 7: Restarting Docker
+	echo Step 9: Restarting Docker
 	systemctl restart docker
 
-	echo Step 8: Enabling Docker
+	echo Step 10: Enabling Docker
 	systemctl enable docker
 
-	echo Step 9: Disabling Firewalld
+	echo Step 11: Disabling Firewalld
 	systemctl disable --now firewalld
 
 	echo I am done :-\)
